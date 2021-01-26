92429039784532343c2afdc24956b59e13cfc0af
heyoni <hhheyoni@gmail.com>
2021-01-26 22:17:12 +0900

    (0126) algo

---
+++ programmers/011.py
@@ -0,0 +1,12 @@
+# 프로그래머스
+# 두 정수 사이의 합
+
+def soulution(a, b):
+    answer = 0
+    if a <= b:
+        for i in range(a, b+1):
+            answer += i
+    else:
+        for i in range(b, a+1):
+            answer += i
+    return answer
