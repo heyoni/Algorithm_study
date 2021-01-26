3e43bf339c469b11673a61833687bcd4892239c8
heyoni <hhheyoni@gmail.com>
2021-01-26 22:31:29 +0900

    (0126) algo

--- programmers/013.py
+++ programmers/013.py
@@ -1,8 +1,5 @@
 # 프로그래머스
 # 문자열 내 p와 y의 개수
 
-# def solution(s, answer):
-#     return s.lower().count('p') == s.lower().count('y')
-
-a = 'AAAbaaaaa'
-print(a.lower().count('a'))
+def solution(s, answer):
+    return s.lower().count('p') == s.lower().count('y')
