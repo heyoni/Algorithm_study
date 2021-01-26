d40343e516947f026b8500df05ba559a1abf5dff
heyoni <hhheyoni@gmail.com>
2021-01-26 22:30:37 +0900

    (0126) algo

--- programmers/011.py
+++ programmers/011.py
@@ -1,7 +1,7 @@
 # 프로그래머스
 # 두 정수 사이의 합
 
-def soulution(a, b):
+def solution(a, b):
     answer = 0
     if a <= b:
         for i in range(a, b+1):
--- programmers/012.py
+++ programmers/012.py
@@ -3,5 +3,3 @@
 
 def solution(strings, n):
     return sorted(strings, key= lambda x:x[n])
-
-print(solution(['sun', 'bed', 'car'], 1))
