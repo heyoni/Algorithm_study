7cad05e34912c9b63f98f1200391390e91a68536
heyoni <hhheyoni@gmail.com>
2021-03-06 00:29:39 +0900

    (0305) algo

--- programmers/59.py
+++ programmers/59.py
@@ -2,17 +2,17 @@ from itertools import combinations
 
 clothes = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 
-# 딕셔너리(해시) 만들기
-d_clothes = dict(clothes)
-temp = []
-answer = len(clothes)
+d_clothes = dict()
+answer = 1
 
-for i in range(2, len(d_clothes)+1):
-    temp.append(list(combinations(d_clothes, i)))
-print(temp)
+for name, kind in clothes:
+    if kind in d_clothes:
+        d_clothes[kind] += 1
 
-# value 값을 가지고 경우의 수 만들기
-# for i in d_clothes:
-    # if d_clothes
-# 같으면 추가할 수 없고
-# 다르면 추가할 수 있음
+    else:
+        d_clothes[kind] = 1
+
+for i in d_clothes.values():
+    answer *= (i + 1)
+
+print(answer)
